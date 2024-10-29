c8bce089a7cff1ed86785be4c4e812fd5119cd84
LahiruViet <48759351+LahiruViet@users.noreply.github.com>
2024-10-29 22:04:02 +0700

    Update OpenAPIConfig.java

--- src/main/java/com/example/order/config/OpenAPIConfig.java
+++ src/main/java/com/example/order/config/OpenAPIConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAPIConfig {
 
     @Bean
-    public OpenAPI productServiceAPI() {
+    public OpenAPI orderServiceAPI() {
 
         return new OpenAPI()
                 .info(new Info().title("Order Service API")
